090756ba174e0ff27631d2c697e2b3a7a27e7c36
Kyle Stankovich <gamemakergml@gmail.com>
2021-06-30 16:53:44 -0400

    Started adding project seeding

---
+++ CopeID.Seeding/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+
+using CopeID;
+
+namespace CopeID.Seeding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var context = new DbContext())
+            {
+                Console.WriteLine("Connected!");
+            }
+        }
+    }
+}
