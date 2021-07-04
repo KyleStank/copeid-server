7cfe32f2e24874d1bc38990362349a11e1a79990
Kyle Stankovich <gamemakergml@gmail.com>
2021-07-04 13:14:16 -0400

    Created controller and service for Specimen model.

---
+++ CopeID.API/Controllers/SpecimenController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using CopeID.API.Models;
+using CopeID.API.Services;
+
+namespace CopeID.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpecimenController : BaseCrudController<Specimen, SpecimenController, ISpecimenService>
+    {
+        public SpecimenController(ILogger<SpecimenController> logger, ISpecimenService specimenService) : base(logger, specimenService) { }
+    }
+}
---
+++ CopeID.API/Services/SpecimenService.cs
@@ -0,0 +1,11 @@
+﻿using CopeID.API.Models;
+
+namespace CopeID.API.Services
+{
+    public interface ISpecimenService : IBaseCrudService<Specimen> { }
+
+    public class SpecimenService : BaseCrudService<Specimen>, ISpecimenService
+    {
+        public SpecimenService(CopeIdDbContext context) : base(context) { }
+    }
+}
