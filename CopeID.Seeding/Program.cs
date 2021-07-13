fe9ec0ff6ffae975481854c65ed07591d3e0b2ee
Kyle Stankovich <gamemakergml@gmail.com>
2021-07-12 22:39:03 -0400

    Added seeding data for Contributors table

--- CopeID.Seeding/Program.cs
+++ CopeID.Seeding/Program.cs
@@ -20,9 +20,13 @@ namespace CopeID.Seeding
         private static readonly string _photographDataFile = _jsonDataDirectory + "PhotographData.json";
         private static readonly string _genusDataFile = _jsonDataDirectory + "GenusData.json";
         private static readonly string _specimenDataFile = _jsonDataDirectory + "SpecimenData.json";
+        private static readonly string _contributorsDataFile = _jsonDataDirectory + "ContributorsData.json";
 
         static async Task Main(string[] args)
         {
+            Console.WriteLine("Reading seeding data...");
+            Console.WriteLine("\n");
+
             // Read JSON data.
             string photographJson = await ReadTextFile(_photographDataFile);
             Photograph[] photographData = JsonConvert.DeserializeObject<Photograph[]>(photographJson);
@@ -51,6 +55,11 @@ namespace CopeID.Seeding
                 specimenData[i].PhotographId = photo.Id;
             }
 
+            string contributorsJson = await ReadTextFile(_contributorsDataFile);
+            Contributor[] contributorsData = JsonConvert.DeserializeObject<Contributor[]>(contributorsJson);
+
+            Console.WriteLine("Creating DB Context...");
+
             // Connect to DB.
             DbContextOptions<CopeIdDbContext> options = new DbContextOptionsBuilder<CopeIdDbContext>()
                 .UseSqlServer("Server=localhost;Database=CopeId;User=sa;Password=Edison15;")
@@ -59,13 +68,29 @@ namespace CopeID.Seeding
             using (CopeIdDbContext context = new CopeIdDbContext(options))
             {
                 context.Database.EnsureCreated();
+                Console.WriteLine("DB context created!");
+                Console.WriteLine("\n");
+                Console.WriteLine("=== Seeding... ===");
 
+                Console.WriteLine(" - Photographs...");
                 await context.Set<Photograph>().AddRangeAsync(photographData);
+
+                Console.WriteLine(" - Genuses...");
                 await context.Set<Genus>().AddRangeAsync(genusData);
+
+                Console.WriteLine(" - Specimens...");
                 await context.Set<Specimen>().AddRangeAsync(specimenData);
+
+                Console.WriteLine(" - Contributors...");
+                await context.Set<Contributor>().AddRangeAsync(contributorsData);
+
+                Console.WriteLine(" === Complete ===");
 
                 context.SaveChanges();
             }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Finishing seeding DB!");
         }
 
         static async Task<string> ReadTextFile(string file)
