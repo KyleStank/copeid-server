d8a31288d28468a4400b7f76ebd80221d6bdefc4
Kyle Stankovich <gamemakergml@gmail.com>
2021-08-25 21:31:58 -0400

    Added various different filter-related models and migrations

--- CopeID.Context/CopeIdDbContext.cs
+++ CopeID.Context/CopeIdDbContext.cs
@@ -6,17 +6,23 @@ namespace CopeID.Context
 {
     public class CopeIdDbContext : DbContext
     {
-        public DbSet<Genus> Genuses { get; set; }
-
-        public DbSet<Photograph> Photographs { get; set; }
-
-        public DbSet<Specimen> Specimens { get; set; }
-
         public DbSet<Contributor> Contributors { get; set; }
 
         public DbSet<Definition> Definitions { get; set; }
 
+        public DbSet<Filter> Filters { get; set; }
+
+        public DbSet<FilterSection> FilterSections { get; set; }
+
+        public DbSet<FilterSectionOption> FilterSectionOptions { get; set; }
+
+        public DbSet<Genus> Genuses { get; set; }
+
+        public DbSet<Photograph> Photographs { get; set; }
+
         public DbSet<Reference> References { get; set; }
+
+        public DbSet<Specimen> Specimens { get; set; }
 
         public CopeIdDbContext(DbContextOptions<CopeIdDbContext> options) : base(options) { }
     }
---
+++ CopeID.Models/Filter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CopeID.Models
+{
+    public class Filter : Entity
+    {
+        [Required]
+        public string EntityType { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; }
+
+        public virtual ICollection<FilterSection> Sections { get; set; }
+    }
+
+    public class FilterSection : Entity
+    {
+        [Required]
+        public Guid FilterId { get; set; }
+
+        [Required]
+        public string EntityProperty { get; set; }
+
+        [Required]
+        public FilterSectionEntityPropertyType EntityPropertyType { get; set; }
+
+        [Required]
+        public string Code { get; set; } // L
+
+        [Required]
+        public string DisplayName { get; set; } // Length of Specimen (mm)
+
+        public virtual Filter Filter { get; set; }
+
+        public virtual ICollection<FilterSectionOption> Options { get; set; } // (0.5, Z), (1, Y), (1.5, X), (2, W)
+    }
+
+    public enum FilterSectionEntityPropertyType
+    {
+        String = 0,
+        Boolean = 1,
+        Integer = 2,
+        Decimal = 3
+    }
+
+    public class FilterSectionOption : Entity
+    {
+        [Required]
+        public Guid FilterSectionId { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; }
+
+        [Required]
+        public string Value { get; set; } // 0.5
+
+        public virtual FilterSection Section { get; set; }
+    }
+}
