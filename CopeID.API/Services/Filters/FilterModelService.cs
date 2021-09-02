cced7af4f3e48e13468b8116fef42569b7a6ead8
Kyle Stankovich <gamemakergml@gmail.com>
2021-09-02 09:16:21 -0400

    Added a couple endpoints to FilterModelController to return the reflected PropertyType and actual FilterModelProperty arrays of a specified ID of FilterModel

--- CopeID.API/Services/Filters/FilterModelService.cs
+++ CopeID.API/Services/Filters/FilterModelService.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 
+using Microsoft.EntityFrameworkCore;
+
+using CopeID.Core.Exceptions;
 using CopeID.Context;
 using CopeID.Models;
 using CopeID.Models.Filters;
@@ -20,5 +25,28 @@ namespace CopeID.API.Services.Filters
                 .Where(t => !t.IsAbstract && t.IsClass && t.IsSubclassOf(_entityType))
                 .OrderBy(t => t.FullName)
                 .ToList();
+
+        public async Task<IEnumerable<FilterModelProperty>> GetProperties(Guid id)
+        {
+            FilterModel model = await FindEntityAsync(id, _set.AsNoTracking().Include(x => x.FilterModelProperties));
+            if (model == null) throw new EntityNotFoundException<FilterModel>();
+
+            return model.FilterModelProperties;
+        }
+
+        public async Task<IEnumerable<PropertyInfo>> GetTypePropertyInfos(Guid id)
+        {
+            FilterModel model = await GetUntrackedAsync(id);
+            if (model == null) throw new EntityNotFoundException<FilterModel>();
+
+            string typeName = model.TypeName;
+            if (typeName == null) throw new EntityException<FilterModel>("Type name is null");
+
+            List<Type> entityTypes = GetEntityTypes();
+            Type type = entityTypes.FirstOrDefault(t => t.FullName == typeName);
+            if (type == null) throw new EntityNotFoundException<FilterModel>($"Type [${typeName}] does not exist");
+
+            return type.GetProperties();
+        }
     }
 }
--- CopeID.API/Services/Filters/IFilterModelService.cs
+++ CopeID.API/Services/Filters/IFilterModelService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
 
 using CopeID.Models.Filters;
 
@@ -8,5 +10,8 @@ namespace CopeID.API.Services.Filters
     public interface IFilterModelService : IBaseEntityService<FilterModel>
     {
         List<Type> GetEntityTypes();
+
+        Task<IEnumerable<FilterModelProperty>> GetProperties(Guid id);
+        Task<IEnumerable<PropertyInfo>> GetTypePropertyInfos(Guid id);
     }
 }
