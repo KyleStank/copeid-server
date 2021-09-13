93aa9d3e4f84ddc5df5abcc209ca536d0742cad4
Kyle Stankovich <gamemakergml@gmail.com>
2021-09-12 22:15:27 -0400

    Added FilterModelPropertyId to FilterSection and created migration

---
+++ CopeID.Context/Migrations/20210913015239_AddFilterModelPropertyIdToFilterSection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CopeID.Context.Migrations
+{
+    public partial class AddFilterModelPropertyIdToFilterSection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "FilterSections",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "FilterSections",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FilterModelPropertyId",
+                table: "FilterSections",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FilterSections_FilterModelPropertyId",
+                table: "FilterSections",
+                column: "FilterModelPropertyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FilterSections_FilterModelProperties_FilterModelPropertyId",
+                table: "FilterSections",
+                column: "FilterModelPropertyId",
+                principalTable: "FilterModelProperties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FilterSections_FilterModelProperties_FilterModelPropertyId",
+                table: "FilterSections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FilterSections_FilterModelPropertyId",
+                table: "FilterSections");
+
+            migrationBuilder.DropColumn(
+                name: "FilterModelPropertyId",
+                table: "FilterSections");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "FilterSections",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "FilterSections",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- CopeID.Models/Filters/FilterSection.cs
+++ CopeID.Models/Filters/FilterSection.cs
@@ -9,13 +9,15 @@ namespace CopeID.Models.Filters
         [Required]
         public Guid FilterId { get; set; }
 
-        [Required]
+        public Guid? FilterModelPropertyId { get; set; }
+
         public string Code { get; set; } // L
 
-        [Required]
         public string DisplayName { get; set; } // Length of Specimen (mm)
 
         public virtual Filter Filter { get; set; }
+
+        public virtual FilterModelProperty FilterModelProperty { get; set; }
 
         public virtual ICollection<FilterSectionOption> FilterSectionOptions { get; set; } // (0.5, Z), (1, Y), (1.5, X), (2, W)
     }
